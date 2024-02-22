74f211c0f3c62982d3af4a09913355a5eb9b5c70
Yann <yanngames68@gmail.com>
2024-02-22 15:59:21 -0300

    Ajuste do Summary

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,4 +1,3 @@
-import { useMemo } from 'react'
 import { TransactionContext } from '../contexts/TransactionsContext'
 import { useContextSelector } from 'use-context-selector'
 
@@ -7,26 +6,24 @@ export function useSummary() {
     return context.transactions
   })
 
-  const summary = useMemo(() => {
-    transactions.reduce(
-      (acc, transaction) => {
-        if (transaction.type === 'income') {
-          acc.income += transaction.price
-          acc.total += transaction.price
-        } else {
-          acc.outcome += transaction.price
-          acc.total -= transaction.price
-        }
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price
+        acc.total += transaction.price
+      } else {
+        acc.outcome += transaction.price
+        acc.total -= transaction.price
+      }
 
-        return acc
-      },
-      {
-        income: 0,
-        outcome: 0,
-        total: 0,
-      },
-    )
-  }, [transactions])
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  )
 
   return summary
 }
